75ba0cb17cad0adca2a2144eb1b1dd0f04678abd
Oksana-Metushi <oksana.metushi@yahoo.com>
2024-03-29 08:44:36 +0100

    changes3

--- project-client/src/pages/home/Brands.jsx
+++ project-client/src/pages/home/Brands.jsx
@@ -2,24 +2,28 @@ import React from 'react'
 import img from "../../../public/images/home/brands.jpg"
 
 const brandlist = [
-    { id: 1, title: "Apple", des: "Delight your guests with our flavors and  presentation", img: img },
-    { id: 2, title: "Samsung", des: "We deliver your order promptly to your door", img: img },
+    { id: 1, title: "Apple", des: "Delight your guests with our flavors and  presentation", img: img, desc: "Designed to be loved." },
+    { id: 2, title: "Samsung", des: "We deliver your order promptly to your door", img: img, desc: "Epic, just like that." },
 ]
 
 const Brands = () => {
     return (
-        <div className='grid md:grid-cols-2 sm:grid-cols-1 gap-4 section-container md:justify-between my-[10%]'>
+        <div className='section-container my-[10%]'>
+            <h1 className='font-tek text-3xl text-slate-400 mb-10 '>Brands</h1>
+        <div className='grid md:grid-cols-2 sm:grid-cols-1 gap-4 md:justify-between '>
             {
                 brandlist.map((brand) => (
                     <div className="card bg-base-100 shadow-xl image-full">
                     <figure><img src={brand.img} alt="phones" /></figure>
                     <div className="card-body">
-                        <h2 className="font-tek text-5xl text-slate-400 mt-10">{brand.title}</h2>
+                        <h2 className="font-tek text-5xl text-slate-400 my-10">{brand.title}</h2>
+                        <p className='text-slate-300 font-serif leading-loose text-3xl text-right'>{brand.desc}</p>
                     </div>
                 </div>
                 ))
             }
            
+        </div>
         </div>
 
     )
